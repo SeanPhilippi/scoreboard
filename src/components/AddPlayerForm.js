2f7d0ea8c5a55c40050f6c964cd2ebe3a12ba5e8
SeanPhilippi <sean.philippi@gmail.com>
2018-11-01 19:01:46 -0500

    comment added

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 class AddPlayerForm extends React.Component {
-
+   // ref used so render doesn't have to be called upon every keypress
+   // instead, called upon submit now.  using state here was bulky vs using ref
    playerInput = React.createRef();
 
    handleSubmit = (e) => {
