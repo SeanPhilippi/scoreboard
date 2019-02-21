27a2ab18d897c8e01ee04b8b26e189f4efa6ced5
Sean Philippi <sean.philippi@gmail.com>
2019-02-20 20:16:42 -0600

    Updates

---
+++ precache-manifest.b5f4a14d62e0470e9c41bd14948e7c87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1b41325083169089dd",
+    "url": "/scoreboard/static/js/runtime~main.dc1b4132.js"
+  },
+  {
+    "revision": "c7206974cd3f58069878",
+    "url": "/scoreboard/static/js/main.c7206974.chunk.js"
+  },
+  {
+    "revision": "5151e6c3a76d0de893cc",
+    "url": "/scoreboard/static/js/1.5151e6c3.chunk.js"
+  },
+  {
+    "revision": "c7206974cd3f58069878",
+    "url": "/scoreboard/static/css/main.8e963e09.chunk.css"
+  },
+  {
+    "revision": "a842b1d9425143d5b1420365faeafeee",
+    "url": "/scoreboard/index.html"
+  }
+];
